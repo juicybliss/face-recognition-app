db0a27ce32710fcae6381a64971fb5e7d0cb50d4
Elvia <etcamarena@gmail.com>
2021-09-14 11:58:16 -0700

    connecting endpoints to backend

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ class App extends Component {
   onButtonSubmit = () => {
     console.log('hi elvia. you made this work... finally!')
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://mighty-tor-88895.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -89,7 +89,7 @@ class App extends Component {
       .then(response => {
         console.log('hi, ', response, '!', this .state.user.id);
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://mighty-tor-88895.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
