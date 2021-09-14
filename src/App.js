46c56db7ddcffefb6dadd266cf14c26ac496b4ba
Elvia <etcamarena@gmail.com>
2021-09-13 22:49:49 -0700

    moved api key to back end... again

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
@@ -10,9 +9,7 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
-const app = new Clarifai.App({
-  apiKey: '0795ddaed65f4f6c8e8d12fb1421ca1d'
-})
+
 
 const particlesOptions = {
   particles: {
@@ -56,12 +53,6 @@ class App extends Component {
     }})
   }
 
-  // componentDidMount() {
-  //   fetch('http://localhost:3000')
-  //   .then(response => response.json())
-  //   .then(console.log)
-  // }
-
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
@@ -87,11 +78,16 @@ class App extends Component {
   onButtonSubmit = () => {
     console.log('hi elvia. you made this work... finally!')
     this.setState({imageUrl: this.state.input});
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input)
+      fetch('http://localhost:3000/imageurl', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          input: this.state.input
+        })
+      })
+      .then(response => response.json())
       .then(response => {
-        console.log('hi, ', response, '!')
+        console.log('hi, ', response, '!', this .state.user.id);
         if (response) {
           fetch('http://localhost:3000/image', {
             method: 'put',
@@ -102,16 +98,14 @@ class App extends Component {
           })
           .then(response => response.json())
           .then(count => {
-            this.setState(Object.assign(this.state.user, {entries: count}))
+            this.setState(Object.assign(this.state.user, {entries: count}));
           })
-          .catch(console.log)
+          .catch(console.log, response.json)
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
       .catch(err => console.log(err));
   }
-
-
 
   onRouteChange = (route) => {
     if (route === 'signout') {
